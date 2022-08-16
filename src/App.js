1fe9fa55136dfcf82082f46aa6e1c0f2a9b5212e
probir ghosh <probirghosh@outlook.com>
2022-08-17 03:53:28 +0600

    redux provider implimented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-
+import { Provider } from "react-redux";
 import Counter from "./components/Counter/Counter";
+import store from "./components/redux/store";
 import Stats from "./components/Stats/Stats";
 
 function App() {
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
-      <div class="max-w-md mx-auto mt-10 space-y-5">
-        <Counter/>
-        <Counter/>
-        <Counter/>
-        <Stats count={10}/>
+    <Provider store={store} >
+      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+        <div class="max-w-md mx-auto mt-10 space-y-5">
+          <Counter />
+          <Counter />
+          <Counter />
+          <Stats count={10} />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
