3a16b2fba9e7f73ec5399c6bb8532b8607e8cb7c
probir ghosh <probirghosh@outlook.com>
2022-08-16 16:37:15 +0600

    total count added

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Stats = ({count}) => {
+const Stats = ({count}) => { 
     return (
         <div className="p-4 h-40 flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
             <div className="text-2xl font-semibold">Total count: {count}</div>
