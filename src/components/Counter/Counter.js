d10053587319f7ae3431baba250985d44641c15d
probir ghosh <probirghosh@outlook.com>
2022-08-16 15:14:02 +0600

    starter template

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,16 +2,21 @@ import React from "react";
 
 const Counter = () => {
   return (
-    <div>
-      <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-        <div className="text-2xl font-semibold">0</div>
-        <div className="flex space-x-3">
-          <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow">
-            Increment
-          </button>
-          <button className="bg-red-400 text-white px-3 py-2 rounded shadow">
-            Decrement
-          </button>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+      <div class="max-w-md mx-auto mt-10 space-y-5">
+        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+          <div class="text-2xl font-semibold">0</div>
+          <div class="flex space-x-3">
+            <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+              Increment
+            </button>
+            <button class="bg-red-400 text-white px-3 py-2 rounded shadow">
+              Decrement
+            </button>
+          </div>
         </div>
       </div>
     </div>
