f9baf209aa682c55d69a61d9b0516cf5761e7561
probir ghosh <probirghosh@outlook.com>
2022-08-16 15:36:34 +0600

    minor issue fixed

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,29 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
  
 const Counter = () => {
-
-    const [count,setCount]=useState(0);
-
-    //increment handler function
-    const incrementHandler = () =>{
-        setCount((previousCount)=>previousCount + 1)
-    }
-
-    //decrement handler function
-    const decrementHandler = () =>{
-        setCount((previousCount)=>previousCount - 1)
-    }
-
-
   return (
     <div>
         <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-          <div class="text-2xl font-semibold">{count}</div>
+          <div class="text-2xl font-semibold">0</div>
           <div class="flex space-x-3">
-            <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={incrementHandler}>
+            <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
               Increment
             </button>
-            <button class="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={decrementHandler}>
+            <button class="bg-red-400 text-white px-3 py-2 rounded shadow">
               Decrement
             </button>
           </div>
