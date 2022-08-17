06169ebe5443601ff9df7f5c7a63c783eb972e78
probir ghosh <probirghosh@outlook.com>
2022-08-17 16:44:55 +0600

    increment-decrement issue fixed by using useDispatch hook

--- src/components/HooksCounter/HooksCounter.js
+++ src/components/HooksCounter/HooksCounter.js
@@ -1,9 +1,18 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import { decrement, increment } from "../redux/counter/actions";
 
 const HooksCounter = ({ id }) => {
 
 const count = useSelector((state)=>state.value)
 
+const dispatch = useDispatch();
+
+const incrementHandler = (value) =>{
+   dispatch(increment(value))
+}
+const decrementHandler = (value) =>{
+   dispatch(decrement(value))
+}
   
   return (
     <div>
@@ -12,13 +21,13 @@ const count = useSelector((state)=>state.value)
         <div class="flex space-x-3">
           <button
             class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-            // onClick={increment}
+            onClick={incrementHandler}
           >
             Increment
           </button>
           <button
             class="bg-red-400 text-white px-3 py-2 rounded shadow"
-            // onClick={decrement}
+            onClick={decrementHandler}
           >
             Decrement
           </button>
